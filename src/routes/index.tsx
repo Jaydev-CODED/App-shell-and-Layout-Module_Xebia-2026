47e2c5ef38face314acfa5646ef3d613b4e4877b
posiedon123 <jaydevswin33@gmail.com>
2026-07-09 13:57:50 +0530

    fix: integrate AuraShell layout with router and tests on integration branch

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -38,7 +38,7 @@ export const router = createBrowserRouter([
             path: '/',
             element: (
               <PageLoader>
-                <Dashboard />
+                <Dashboard onNotify={() => {}} />
               </PageLoader>
             ),
           },
